475f86c13511c839c0471a705c7c0d8717615266
Wassim Akkari <was.akkari@gmail.com>
2023-05-19 14:38:59 +0100

    Implement VoitureController and its tempaltes from TODO-N2.

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -15,4 +15,28 @@ class VoitureController extends AbstractController
             'controller_name' => 'VoitureController',
         ]);
     }
+
+    #[Route('/voiture/affiche', name: 'affiche_voiture')]
+    public function afficheVoiture(): Response
+    {
+        $voitures = array(array('id'=>1, 'marque'=>'BMW', 'Vitesse'=>240, 'puissance'=>10),
+            array('id'=>2, 'marque'=>'Ford', 'Vitesse'=>200, 'puissance'=>6),
+            array('id'=>3, 'marque'=>'Audi', 'Vitesse'=>240, 'puissance'=>8),
+            array('id'=>4, 'marque'=>'Peugeot', 'Vitesse'=>200, 'puissance'=>4));
+
+        return $this->render('voiture/affiche.html.twig', [
+            'controller_name' => 'VoitureController',
+            'listV' => $voitures,
+        ]);
+    }
+
+    #[Route('/voiture/affiche/{vitesse}/{puissance}', name: 'affiche_voiture_vp')]
+    public function afficheVoitureVP($vitesse, $puissance): Response
+    {
+        return $this->render('voiture/affichepv.html.twig', [
+            'controller_name' => 'VoitureController',
+            'vitesse' => $vitesse,
+            'puissance' => $puissance,
+        ]);
+    }
 }
